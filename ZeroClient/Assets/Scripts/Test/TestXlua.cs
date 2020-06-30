7b228eb849d195e4e7c360b2089f4b9e4d423ac9
zxlin <1123302138@qq.com>
2020-06-30 23:43:59 +0800

    add XLua

---
+++ ZeroClient/Assets/Scripts/Test/TestXlua.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+[Hotfix]
+public class TestXlua : MonoBehaviour
+{
+    LuaEnv luaEnv = new LuaEnv();
+    private void Update() {
+        Debug.Log("<<<<<<<<<");
+    }
+
+    private void OnGUI() {
+        if(GUILayout.Button("XLua")){
+            luaEnv.DoString(@"
+                xlua.hotfix(CS.TestXlua, 'Update', function(self)
+                    print('>>>>>>>>>>>>')
+                end)"
+            );
+        }
+    } 
+}
